3f81cc2df77aa69a2ef2e2e1e993f7aabfee467c
yassine <Yassinedhahak@outlook.com>
2025-04-14 19:55:19 +0200

    Update test.h struct de jozed stc

--- test.h
+++ test.h
@@ -9,77 +9,213 @@
 
 
 #define PVMAX 120
-#define ENNERGY 10
+#define ENNERGIE 10
+
 
 typedef struct{
-  char* name;
   float pv; //50<x<100
   float pv_max; //120
   float def; // 0.1<x<0.5 pourcentage de reduction des degat
   float att;// 3<x<8  
-  int speed; // vitesse de regen d ennergie 
+  int speed; // probabilite de rejouer 
   float dodge;  // 0.1<x<0.5 probabilite de dodge
-  int ennergie ; // 10 de base
+}Stats;
 
-  char* crit_name1 ; //nom de l attaque critique
-  float crit1; // 0.1<x<1 multiplicateur des degats
-  float crit_conso1; // la consommation d ennergie de l attaque speciale 1 : entre 1 et 3
 
-  char* crit_name2 ; //nom de l attaque critique
-  float crit2; // 0.1<x<1 multiplicateur des degats
-  float crit_conso2; // la consommation d ennergie de l attaque speciale 2 : entre 2 et 6
 
+typedef struct {
+    int pv;
+    int att;
+    int def;
+    int speed;
+    int dodge;
+} Effect;
+
+typedef struct{
+  char* name;
+  float valeur;
+  char description[200];
+  int tour_actif;
+  int recharge;
+  Effect buff;
+} Special;
+
+
+typedef struct{
+  char* name;
+  Stats stats;
+  Stats stats_temp;
+  Special atk_spe;
+  int tour_buff_restant;
+   
 }Player;
 
 
-//typedef enum{Bouclier, Epee, Mitraillette, Baton, Medkit ,Eclair , None}Assets; // enumeration des assets
+Player p[12] = {  //liste de tout les personnages 
+  {
+    "luffy",                    /// nom du perso
+    {70, PVMAX, 0.3, 7, 2, 0.4},   /// stats du perso
+    {70, PVMAX, 0.3, 7, 2, 0.4}, /// stats temporaire du perso 
+    {"Gum Gum no Gum Gum", 1.4, "", 2, 0, {0, 0, 0, 0, 0}},  /// l'attaque spéciale et ses attributs 
+    0
+  },
+  
+  {
+    "sanji",
+    {60, PVMAX, 0.4, 8, 3, 0.2},
+    {60, PVMAX, 0.4, 8, 3, 0.2},
+    {"diabolo jumbo", 1.2, "", 2, 0, {0, 0, 0, 0, 0}},
+    0
+  },
+  
+  {
+    "zoro",
+    {80, PVMAX, 0.1, 7, 4, 0.3},
+    {80, PVMAX, 0.1, 7, 4, 0.3},
+    {"tiger slash", 1.1, "", 2, 0, {0, 0, 0, 0, 0}},
+    0
+  },
+  
+  {
+    "ace",
+    {90, PVMAX, 0.3, 5, 1, 0.5},
+    {90, PVMAX, 0.3, 5, 1, 0.5},
+    {"Flame fence", 1.2, "", 1, 0, {0, 0, 0, 0, 0}},
+    0
+  },
+
+  {
+    "brook",
+    {70, PVMAX, 0.4, 5, 4, 0.3},
+    {70, PVMAX, 0.4, 5, 4, 0.3},
+    {"Song slash", 1.2, "", 3, 0, {0, 0, 0, 0, 0}},
+    0
+  },
+  {
+    "ussop",
+    {80, PVMAX, 0.3, 5, 3, 0.4},
+    {80, PVMAX, 0.3, 5, 3, 0.4},
+    {"Skull bomb blast", 1.1, "", 1, 0, {0, 0, 0, 0, 0}},
+    0
+  },
+
+  {
+    "do flamingo",
+    {60, PVMAX, 0.4, 5, 5, 0.3},
+    {60, PVMAX, 0.4, 5, 5, 0.3},
+    {"Fallbright", 1.2, "", 2, 0, {0, 0, 0, 0, 0}},
+    0
+  },
+
+  {
+    "big mom",
+    {80, PVMAX, 0.3, 6, 1, 0.5},
+    {80, PVMAX, 0.3, 6, 1, 0.5},
+    {"Master cannon", 1.3, "", 1, 0, {0, 0, 0, 0, 0}},
+    0
+  },
+
+  {
+    "katakuri",
+    {80, PVMAX, 0.4, 8, 1, 0.2},
+    {80, PVMAX, 0.4, 8, 1, 0.2},
+    {"Mochi hadan", 1.3, "", 3, 0, {0, 0, 0, 0, 0}},
+    0
+  },
+  
+  {
+    "black beard",
+    {80, PVMAX, 0.1, 7, 4, 0.3},
+    {80, PVMAX, 0.1, 7, 4, 0.3},
+    {"Black hole", 1.4, "", 2, 0, {0, 0, 0, 0, 0}},
+    0
+  },
+  
+  {
+    "crocodile",
+    {60, PVMAX, 0.5, 4, 5, 0.3},
+    {60, PVMAX, 0.5, 4, 5, 0.3},
+    {"Desert spada", 1.2, "", 3, 0, {0, 0, 0, 0, 0}},
+    0
+  },
+  
+  {
+    "Kaido",                           
+    {90, PVMAX, 0.3, 8, 4, 0.2},
+    {90, PVMAX, 0.3, 8, 4, 0.2},
+    {"Thunder", 1.4, "", 2, 0, {0, 0, 0, 0, 0}},
+    0
+  }
+};
 
 
 
- // declaration des joueurs ( a chager par les files ) 
-Player p1 = {"luffy" , 70 , PVMAX, 0.3 , 7 , 2 , 0.4 , ENNERGY , "Gum Gum no Gum Gum" , 1.4 , 2 , "Gear 5" , 1.8 , 4 };
-Player p2 = {"sanji", 60, PVMAX, 0.4, 8, 3, 0.2, ENNERGY, "diabolo jumbo", 1.2, 2, "bien cuit grill shot", 1.4, 4};
-Player p3 = {"zoro", 80, PVMAX, 0.1, 7, 4, 0.3, ENNERGY, "tiger slash", 1.1, 2, "oni giri", 1.2, 4};
-Player p4 = {"ace", 90, PVMAX, 0.3, 5, 1, 0.5, ENNERGY, "Flame fence", 1.2, 1, "Heat haze", 1.4, 2};
-Player p5 = {"brook", 70, PVMAX, 0.4, 5, 4, 0.3, ENNERGY, "Song slash", 1.2, 3, "blizzard slice", 1.4, 6};
-Player p6 = {"ussop", 80, PVMAX, 0.3, 5, 3, 0.4, ENNERGY, "Skull bomb blast", 1.1, 1, "Platanus Shuriken", 1.2, 2};
-Player p7 = {"do flamingo", 60, PVMAX, 0.4, 5, 5, 0.3, ENNERGY, "Fallbright", 1.2, 2, "String saw", 1.4, 4};
-Player p8 = {"big mom", 80, PVMAX, 0.3, 6, 1, 0.5, ENNERGY, "Master cannon", 1.3, 1, "Mama raid", 1.6, 2};
-Player p9 = {"katakuri", 80, PVMAX, 0.4, 8, 1, 0.2, ENNERGY, "Mochi hadan", 1.3, 3, "Zan Giri Mochi", 1.6, 6};
-Player p10 = {"black beard", 80, PVMAX, 0.1, 7, 4, 0.3, ENNERGY, "Black hole", 1.4, 2, "Black vortex", 1.8, 4};
-Player p11 = {"crocodile", 60, PVMAX, 0.5, 4, 5, 0.3, ENNERGY, "Desert spada", 1.2, 3, "Ground death", 1.4, 6};
-Player p12 = {"Kaido", 90, PVMAX, 0.3, 8, 4, 0.2, ENNERGY, "Thunder", 1.4, 2, "Blast breath", 1.8, 4};
+void vraiatout(Player* p, int a) { //change les stats des perso
+  switch(a) {
+      case 1 : //Bouclier
+          p->stats_temp.def += 0.1;
+          p->stats_temp.dodge -= 0.1;
+          break;
+      case 2: //Epee
+          p->stats_temp.att += 0.1;
+          p->stats_temp.def -= 0.1;
+          break;
+      case 3: //Mitraillette
+          p->stats_temp.pv -= 10;
+          break;
+      case 4: //Baton
+          p->stats_temp.dodge += 0.1;
+          break;
+      case 5: //medkit
+          p->stats_temp.pv += 10;
+          p->stats_temp.att -= 0.1;
+          break;
+      case 6: //Eclair
+          p->stats_temp.speed += 1;
+          p->stats_temp.att -= 0.1;
+          break;
+      case 0 : //none
+          break;
+      default:
+          printf("Erreur : atout non reconnu\n");
+          exit(1);
+  }
 
+  // Sécurité : éviter de dépasser les limites si nécessaire
+  if (p->stats_temp.pv > p->stats.pv_max) {
+      p->stats_temp.pv = p->stats.pv_max;
+  }
+}
 
 
 
 
 typedef struct {
-    char* nom;
+  char* nom;
     int pv;
     int att;
     int def;
     int speed;
     int dodge;
     int crit;
-} Personnage;
+} AffichagePerso;
 
-Personnage persos[12] = {  //pour l affichage 
-    {"(1)  Luffy", 2, 4, 3, 2, 4, 4},
-    {"(2)  Sanji", 1, 5, 4, 3, 2, 2},
-    {"(3)  Zoro", 3, 4, 1, 4, 3, 1},
-    {"(4)  Ace", 4, 2, 3, 1, 5, 2},
-    {"(5)  Brook", 2, 2, 4, 4, 3, 2},
-    {"(6)  Ussop", 3, 2, 3, 3, 4, 1},
-    {"(7)  Do Flamingo", 1, 2, 4, 5, 3, 2},
-    {"(8)  Big Mom", 3, 3, 3, 1, 5, 3},
-    {"(9)  Katakuri", 3, 5, 4, 1, 2, 3},
-    {"(10) Black Beard", 3, 4, 1, 4, 3, 4},
-    {"(11) Crocodile", 1, 1, 5, 5, 3, 2},
-    {"(12) Kaido", 4, 5, 3, 4, 2, 4}
+AffichagePerso persos_affichage[12] = { // struct pour l affichage des barres
+  {"(1)  Luffy", 2, 4, 3, 2, 4},
+  {"(2)  Sanji", 1, 5, 4, 3, 2},
+  {"(3)  Zoro", 3, 4, 1, 4, 3},
+  {"(4)  Ace", 4, 2, 3, 1, 5},
+  {"(5)  Brook", 2, 2, 4, 4, 3},
+  {"(6)  Ussop", 3, 2, 3, 3, 4},
+  {"(7)  Do Flamingo", 1, 2, 4, 5, 3},
+  {"(8)  Big Mom", 3, 3, 3, 1, 5},
+  {"(9)  Katakuri", 3, 5, 4, 1, 2},
+  {"(10) Black Beard", 3, 4, 1, 4, 3},
+  {"(11) Crocodile", 1, 1, 5, 5, 3},
+  {"(12) Kaido", 4, 5, 3, 4, 2}
 };
 
-void atout(Personnage *p, int a) {
+void atout(AffichagePerso *p, int a) {
     switch (a) {
         case 1: p->def++; p->dodge--; break;
         case 2: p->att++; p->def--; break;
